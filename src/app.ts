2be948327447a0395535cbe8becfd1460d592532
beskibeski <benladen1917@inbox.ru>
2024-10-21 19:39:56 +0300

    feat: get method

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,39 @@
 import http from 'http';
+import { showPageNotFoundMessage, showServerSideProblem } from './services/create-error-responses';
+import deleteSlashes from './services/delete-extra-char';
+import { getUser, getUsers, postNewUser, changeUser, deleteUser } from './services/users-functions';
 
 let PORT = process.env.PORT;
+
+const PATHS = {
+  API: 'api',
+  USERS: 'users',  
+}
 
 const crud_api = {  
   start(): void {
     const server = http.createServer((req, res) => { 
-      res.setHeader('Content-type', 'application/json');         
+      res.setHeader('Content-type', 'application/json');
+      let url = deleteSlashes(req.url);
+      console.log(url)
+      switch (req.method) {
+        case 'GET':
+          if (url === `${PATHS.API}/${PATHS.USERS}`) {
+            getUsers(res).catch(() => {
+              showServerSideProblem(res);
+              return;
+            });
+          } else if (url.split('/').length === 3 && url.startsWith(`${PATHS.API}/${PATHS.USERS}`)) {
+            const userId = url.split('/')[2];
+            getUser(res, userId).catch(() => {
+              showServerSideProblem(res);
+              return;
+            })
+          } else {
+            showPageNotFoundMessage(res);
+          }
+        break;
+      }        
     })
     server.listen(PORT).on('listening', () => {
       console.log(`Server starts on ${PORT} port`);
